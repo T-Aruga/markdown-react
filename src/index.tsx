421b0ca5ce2d7a2dbaecad2eca4892ad80c4ae57
T-Aruga <baideneo@gmail.com>
2021-01-18 01:59:27 +0900

    Add react-router and header conmponent

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import { createGlobalStyle } from 'styled-components'
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 import { Editor } from './pages/editor'
+import { History } from './pages/history'
 
 
 const GlobalStyle = createGlobalStyle`
@@ -13,7 +20,15 @@ const GlobalStyle = createGlobalStyle`
   const Main = (
     <>
       <GlobalStyle />
-      <Editor />
+      <Router>
+        <Route exact path="/editor">
+          <Editor />
+        </Route>
+        <Route exact path="/history">
+          <History />
+        </Route>
+        <Redirect to="/editor" path="*" />
+      </Router>
     </>
   )
 
